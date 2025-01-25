8e8c83d676a18b7c7b6b811e3c275c82d3fdcf4e
jeongkichang <light-hearted@naver.com>
2025-01-25 10:07:24 +0900

    refact: delete unused codes

--- apps/etl/src/main.ts
+++ apps/etl/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { EtlModule } from './etl.module';
-import {EtlService} from "./etl.service";
 
 async function bootstrap() {
     const app = await NestFactory.create(EtlModule);
