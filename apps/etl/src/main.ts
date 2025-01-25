25c1875ddff0c5ca77db5db052409eadfe18a185
jeongkichang <light-hearted@naver.com>
2025-01-25 10:07:02 +0900

    refact: delete temporary execute codes

--- apps/etl/src/main.ts
+++ apps/etl/src/main.ts
@@ -5,12 +5,5 @@ import {EtlService} from "./etl.service";
 async function bootstrap() {
     const app = await NestFactory.create(EtlModule);
     await app.init();
-
-    const etlService = app.get(EtlService);
-    // await etlService.handleCronJob();
-    // await etlService.crawlPoolData();
-    // await etlService.copySeoulPools();
-    // await etlService.refineSeoulPoolsInfo();
-    // await etlService.getText();
 }
 bootstrap();
