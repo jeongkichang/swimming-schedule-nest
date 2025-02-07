536a4fe3cfbd5087c8b68fe315b00ec9bdaea964
jeongkichang <light-hearted@naver.com>
2025-02-07 19:35:54 +0900

    refact: add available_html_crawling and available_daily_swimming property for seoulPoolInfoSchema and delete unavailable daily swim in daily_swim_schedule collection

--- apps/etl/src/main.ts
+++ apps/etl/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { EtlModule } from './etl.module';
+import { EtlService } from "./etl.service";
 
 async function bootstrap() {
     const app = await NestFactory.create(EtlModule);
     await app.init();
+
+    const etlService = app.get(EtlService);
+    await etlService.updateAvailableDailySwimming();
+    await etlService.deleteDailySwimScheduleByPoolCodes();
+    await etlService.updateAvailableHtmlCrawling();
 }
 bootstrap();
